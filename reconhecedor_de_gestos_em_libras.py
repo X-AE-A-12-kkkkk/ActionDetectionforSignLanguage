4556faa05f5077aa42275e629d5ecaa13f058276
Davi <daviagrm@gmail.com>
2022-07-27 19:52:00 -0400

    modified:   reconhecedor_de_gestos_em_libras.py

--- reconhecedor_de_gestos_em_libras.py
+++ reconhecedor_de_gestos_em_libras.py
@@ -17,12 +17,24 @@ def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+ 
 
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('Detector', frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        image, results = mediapipe_detection(frame, holistic)
+        draw_landmarks(frame, results)
+
+        cv2.imshow('Detector', frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+
 cv2.destroyAllWindows()
